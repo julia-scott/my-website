d2793c3030405f3a8bd4a0349e658df457031dd9
jjscott1 <jjscott@uwaterloo.ca>
2021-06-25 00:07:27 -0400

    Footer positioning

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
+export class FooterComponent implements OnInit{
+	
+	constructor() {
+	}
 
   ngOnInit(): void {
+    if (document.body.scrollHeight > document.body.clientHeight) {
+      document.getElementById("footer")?.setAttribute('style','position:sticky');
+    }
   }
 
 }
