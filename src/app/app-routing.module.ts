1b42d7b189aeb1838e6b83526a7b6fb87b9f7f9f
jjscott1 <jjscott@uwaterloo.ca>
2021-07-22 13:00:28 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { CapstoneComponent } from './capstone/capstone.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home'},
   { path: 'home', component: HomePageComponent },
-  { path: 'about', component: AboutPageComponent }
+  { path: 'about', component: AboutPageComponent },
+  { path: 'castra', component: CapstoneComponent }
 ];
 
 @NgModule({
